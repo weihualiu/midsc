53aa2aea165eb5907f9ca82c05ce61d642afedf1
root <root@localhost.localdomain>
2018-06-16 12:09:14 +0800

    增加编译脚本

---
+++ make.sh
@@ -0,0 +1,14 @@
+##############################
+# File Name: make.sh
+# Author: liu.weihua
+# email: 394806487@qq.com
+# Created Time: 07/21/2018
+##############################
+#!/bin/sh
+
+rm -rf build;
+mkdir build;
+cd build;
+cmake ../;
+make;
+
