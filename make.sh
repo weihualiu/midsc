70684b7603053d500ea86e8cc9c6d210515be912
weihua.liu <auhiewuil@gmail.com>
2018-06-16 12:47:57 +0800

    增加带参数的cmakelists.txt

--- make.sh
+++ make.sh
@@ -2,13 +2,27 @@
 # File Name: make.sh
 # Author: liu.weihua
 # email: 394806487@qq.com
-# Created Time: 07/21/2018
+# Created Time: 06/16/2018
 ##############################
 #!/bin/sh
 
-rm -rf build;
-mkdir build;
-cd build;
-cmake ../;
-make;
+usage(){
+	echo "Usage: $0 -sdbcdir"	
+}
 
+compile(){
+	echo $1
+	rm -rf build;
+	mkdir build;
+	cd build;
+	cmake -DSDBCDIR=$1 ../;
+	make;
+}
+
+case $1 in
+	-sdbcdir)
+		compile $2;
+		;;
+	*)
+		usage;
+	esac
